fc5af487e8637944f2fb3cc7b29a433b777c4266
Phil Adams <phil@philadams.net>
2013-02-25 17:18:23 -0500

    add db connection

--- app.php
+++ app.php
@@ -1,11 +1,26 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+#################
+# CONFIG STUFFS #
+#################
+
+error_reporting(E_ALL);
+define('DEBUG', 1);
+
+$config = json_decode(file_get_contents('./config.json'));  # app config
+require_once __DIR__.'/vendor/autoload.php';  # silex
+require_once './util/functions.php';
+$dbh = db_connect($config->db);
+
+###################
+# THE APPLICATION #
+###################
 
 $app = new Silex\Application();
 
-$app->get('/', function() {
-  return 'viztag v0.0.1 homepage';
+$app->get('/', function() use ($dbh) {
+  //debug($dbh);
+  return 'viztag v0.0.1';
 });
 
 $app->get('/hello', function() {
