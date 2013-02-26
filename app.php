6ef100920ebdb52ce2c83e88c52fb626b158be29
Phil Adams <phil@philadams.net>
2013-02-25 20:56:29 -0500

    login/logout with sessions and twig-based templates

--- app.php
+++ app.php
@@ -16,15 +16,53 @@ $dbh = db_connect($config->db);
 # THE APPLICATION #
 ###################
 
+use Symfony\Component\HttpFoundation\Request;
 $app = new Silex\Application();
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app['debug'] = true;
 
-$app->get('/', function() use ($dbh) {
-  //debug($dbh);
-  return 'viztag v0.0.1';
+$app->get('/', function() use ($app) {
+  $toview = array('help_person' => 'phil');
+  return $app['twig']->render('index.twig', $toview);
 });
 
-$app->get('/hello', function() {
-  return 'Hello!';
+# display the log in form
+$app->get('/login', function() use ($app) {
+  return $app['twig']->render('login.twig');
+});
+
+# log the user out
+$app->get('/logout', function() use ($app) {
+  $app['session']->set('user', null);
+  return $app->redirect('/viztag');
+});
+
+# log in a coder with username/password - for now, test/password
+$app->post('/login', function(Request $request) use ($app) {
+  $username = $request->get('username');
+  $password = $request->get('password');
+  if ($username == 'test' && $password == 'password') {
+    $app['session']->set('user', array('username' => $username));
+    return $app->redirect('/viztag');
+  } else {  # try again... TODO add error message
+    return $app->redirect('/viztag/login');
+  }
+});
+
+# load a randomly selected status and display tagging / commenting for it
+$app->get('/tag', function() use ($app) {
+  if (null == $user = $app['session']->get('user')) {
+    return $app->redirect('/viztag/login');
+  }
+  return 'todo';
+});
+
+# persist tagging / commenting for a status
+$app->post('/tag/{id}', function($id) use ($app) {
+  return 'POST tag: todo';
 });
 
 $app->run();
